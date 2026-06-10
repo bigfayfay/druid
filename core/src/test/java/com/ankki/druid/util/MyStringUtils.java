3bca6e2597f70cd287a7e4a07b9c826ea6104672
yangfei <yangfei@ankki.com>
2026-06-10 17:43:02 +0800

    modify: mysql-fix

--- core/src/test/java/com/ankki/druid/util/MyStringUtils.java
+++ core/src/test/java/com/ankki/druid/util/MyStringUtils.java
@@ -3,7 +3,8 @@ package com.ankki.druid.util;
 public class MyStringUtils {
 
     public static String replaceAllInvisibleChars(String str) {
-        return str == null ? null : str.replaceAll("\\u0011", "\\\\")
+        return str == null ? null :
+                str.replaceAll("\\u0011", "\\\\")
                                     .replaceAll("\\u0012", "\\\"")
                                     .replaceAll("\\u0013", "\\\r")
                                     .replaceAll("\\u0014", "\\\n");
